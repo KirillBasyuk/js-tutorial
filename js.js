910d864903a2b69b1629e309e464ca8ea2322971
kirill_basyuk <kirickbasyuk@mail.ru>
2019-12-01 20:49:53 +0300

    updated files

--- js.js
+++
@@ -1,7 +0,0 @@
-let year = document.querySelector('.news__date__year');
-let time = document.querySelector('.news__date__time');
-
-let timerId = setInterval(() => {
-    year.innerHTML = new Date().toLocaleDateString();
-    time.innerHTML = new Date().toLocaleTimeString();
-}, 1000);
